f344c89bd9de0110d9c4eb649d1df86e386256fe
Nadia Carreira nunez <nadiacarreiranunez@gmail.com>
2025-04-13 11:47:08 +0200

    create components structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { IllustrationComponent } from './components/illustration/illustration.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [NavbarComponent, HomeComponent, AboutComponent, PortfolioComponent, IllustrationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'my_portfolio';
+  title = 'Nadia Carreira UX-UI';
 }
---
+++ src/app/components/illustration/illustration.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-illustration',
+  standalone: true,
+  imports: [],
+  templateUrl: './illustration.component.html',
+  styleUrl: './illustration.component.scss'
+})
+export class IllustrationComponent {
+
+}
